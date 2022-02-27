7aeb0cb03bedf39c38713569de2688300a7c010e
sobekbattle <100533770+sobekbattle@users.noreply.github.com>
2022-02-27 11:13:44 -0800

    Update Title Page

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,13 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
+       <Head>
+        <title>Sobek Battle</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Full Contact Crypto" />
       </main>
 
       <Footer />
