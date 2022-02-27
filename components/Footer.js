ecaecf5bd9bf66251217e4b0ffbc35ef1d587f88
sobekbattle <100533770+sobekbattle@users.noreply.github.com>
2022-02-27 11:10:00 -0800

    Adding social links

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        <a href="https://twitter.com/SobekBattle"><img src="/twitter.svg" alt="Twitter" className={styles.logo} /></a>
+        <a href="https://discord.gg/Www7MK43UE"><img src="/discord.svg" alt="Discord" className={styles.logo} /></a>
       </footer>
     </>
   )
